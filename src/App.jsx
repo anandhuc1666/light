76f277232def8c248e0824882a5d677387dd61e0
anandhuc1666 <anandhuc166@gmail.com>
2025-09-09 12:23:59 +0530

    minicode

--- src/App.jsx
+++ src/App.jsx
@@ -1,129 +1,24 @@
-// import React, { useState, useEffect } from "react";
-// import './App.css'
-
-// function App() {
-//   const [active, setActive] = useState("red"); 
-
-//   useEffect(() => {
-//     let interval = setInterval(() => {
-//       setActive((prev) => {
-//         if (prev === "red") return "green";
-//         if (prev === "green") return "yellow";
-//         if (prev === "yellow") return "red";
-//         return "red";
-//       });
-//     }, 2000); 
-
-//     return () => clearInterval(interval);
-//   }, []);
-
-//   return (
-//     <div className="traffic-light">
-//       <div
-//         className={`light ${active === "green" ? "green" : "gray"}`}
-//       ></div>
-//       <div
-//         className={`light ${active === "yellow" ? "yellow" : "gray"}`}
-//       ></div>
-//       <div
-//         className={`light ${active === "red" ? "red" : "gray"}`}
-//       ></div>
-//     </div>
-//   );
-// }
-
-// export default App;
-// import React, { useEffect, useState } from 'react'
-// import "./App.css"
-
-// function App() {
-//   const [active, setActive] = useState("red")
-//   useEffect(() => {
-//     let interval = setInterval(() => {
-//       setActive((pree) => {
-//         if (pree === "red") return "green"
-//         if (pree === "green") return "yellow"
-//         if (pree === "yellow") return "red"
-//         return "red"
-//       })
-//     }, 1000)
-//     return () => clearInterval(interval)
-//   }, [])
-//   return (
-//     <div>
-//       <div className={`light ${active === "green" ? "green" : "gray"}`}></div>
-//       <div className={`light ${active === "yellow" ? "yellow" : "gray"}`}></div>
-//       <div className={`light ${active === "red" ? "red" : "gray"}`}></div>
-
-//     </div>
-//   )
-// }
-
-// export default App
-// import React, { useState } from 'react'
-
-// function App() {
-//   const [text, setText] = useState('')
-//   const [tests, setTexts] = useState([])
-//   const [edits, setEdits] = useState(null)
-//   const send = (e) => {
-//     if (edits !== null) {
-//       const newedit = [...tests]
-//       newedit[edits] = text
-//       setTexts(newedit)
-//       setEdits(null) 
-
-//     } else {
-//       setTexts([...tests, text])
-
-//     }
-//     setText("")
-//   }
-//   const remove = (index) => {
-//     setTexts(tests.filter((p, i) => i !== index))
-//   }
-
-//  const edit = (index) => {
-//     setText(tests[index])   
-//     setEdits(index)         
-//   }
-
-//   return (
-//     <div>
-//       <h1>add todo</h1>
-//       <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
-//       <button onClick={send}>{edits !== null ? "Update" : "Add"}</button>
-//       <ul>
-//         {
-//           tests.map((i, k) => (
-//             <li key={k}>{i}<button onClick={() => remove(k)}>dle</button><button onClick={() => edit(k)}>edit</button></li>
-//           ))
-//         }
-//       </ul>
-//     </div>
-//   )
-// }
-
-// export default App
-import React from 'react'
-import S1 from './context/s1'
-import { Route, Routes } from 'react-router-dom'
-import S2 from './context/s2'
-import P1 from './paramus/P1'
-import P2 from './paramus/P2'
-import O2 from './option/O2'
-
+import React, { useEffect, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [color,setColor]=useState("gray")
+  useEffect(()=>{
+    const interval = setInterval(()=>{
+      setColor ((pree)=>{
+        if(pree==="gray")return "green"
+        if(pree==="green")return "yellow"
+        if(pree==="yellow")return "red"
+        if(pree==="red")return "green"
+      })
+    },1000)
+    return ()=>clearInterval(interval)
+  },[])
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<S1 />} />
-        <Route path='/s2' element={<S2 />} />
-        <Route path='/P1/:text' element={<P1 />} />
-        <Route path='/P2' element={<P2 />} />
-        <Route path='/O2' element={<O2/>}/>
-      </Routes>
+      <div className={`div ${color==="green"?"green":"gray"}`}></div>
+      <div className={`div ${color==="yellow"?"yellow":"gray"}`}></div>
+      <div className={`div ${color==="red"?"red":"gray"}`}></div>
     </div>
   )
 }
