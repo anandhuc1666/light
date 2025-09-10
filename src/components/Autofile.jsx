cb7aeb9e74277fbbcdcd89466d143118f747fd6c
anandhuc1666 <anandhuc166@gmail.com>
2025-09-10 10:29:47 +0530

    miniCode

--- src/components/Autofile.jsx
+++ src/components/Autofile.jsx
@@ -1,23 +1,51 @@
+// import React, { useState } from 'react'
+
+// function Autofile() {
+//     const[text,setText]=useState('')
+//     const data = ["aaa","dafa", "efwvs","sfcszca","eegsfq"] 
+//     let newT = data.filter((t,k)=>t==text)
+//     console.log(data)
+//     console.log(newT)
+//   return (
+//     <div>
+//        <input type="text" placeholder='enter your text' value={text} onChange={(e)=>setText(e.target.value)}/>
+//        <div>
+//          <ul>
+//             {
+//                 newT.map((i,k)=>(
+//                     <li key={k}>{i}</li>
+//                 ))
+//             }
+//          </ul>
+//        </div>
+//     </div>
+//   )
+// }
+
+// export default Autofile
 import React, { useState } from 'react'
 
 function Autofile() {
-    const[text,setText]=useState('')
-    const data = ["aaa","dafa", "efwvs","sfcszca","eegsfq"] 
-    let newT = data.filter((t,k)=>t==text)
-    console.log(data)
-    console.log(newT)
+  const [text, setText] = useState('')
+  const data = ["aaa", "dafa", "efwvs", "sfcszca", "eegsfq"]
+  // let newT = data.filter((t) => t.toLowerCase().includes(text.toLowerCase()))
+  let newT = data.filter((i)=>i.includes(text))
+
   return (
     <div>
-       <input type="text" placeholder='enter your text' onChange={(e)=>setText(e.target.value)}/>
-       <div>
-         <ul>
-            {
-                newT.map((i,k)=>(
-                    <li key={k}>{i}</li>
-                ))
-            }
-         </ul>
-       </div>
+      <input
+        type="text"
+        placeholder="enter your text"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
+      <div>
+        <ul>
+          {newT.map((i, k) => (
+            <li key={k}>{i}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
