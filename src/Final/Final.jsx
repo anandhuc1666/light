20e2fe47c68f1730fdf747ad3eaa10f96d8c8640
anandhuc1666 <anandhuc166@gmail.com>
2025-09-11 10:52:19 +0530

    minicode

--- src/Final/Final.jsx
+++ src/Final/Final.jsx
@@ -2,6 +2,15 @@ import React, { useState } from 'react'
 
 function Final() {
     const [color,setColor]=useState("gray")
+    const value = setInterval(()=>{
+      setColor((value)=>{
+         if(value==="gray")return"green"
+         if(value==="green")return"yellow"
+         if(value==="yellow")return"red"
+         if(value==="red")return"green"
+      })
+      return ()=> clearInterval(value)
+    },1000)
   return (
     <div>
         <div className={`light ${color==="green"?"green":"gray"}`}></div>
