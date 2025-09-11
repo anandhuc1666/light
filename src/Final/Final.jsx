362fdc92187ed936aca95d93c79661158cd14e90
anandhuc1666 <anandhuc166@gmail.com>
2025-09-11 10:59:43 +0530

    miniCode

--- src/Final/Final.jsx
+++ src/Final/Final.jsx
@@ -1,21 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Final() {
-    const [color,setColor]=useState("gray")
-    const value = setInterval(()=>{
-      setColor((value)=>{
-         if(value==="gray")return"green"
-         if(value==="green")return"yellow"
-         if(value==="yellow")return"red"
-         if(value==="red")return"green"
+  const [color, setColor] = useState("gray")
+  useEffect(() => {
+    const value = setInterval(() => {
+      setColor((value) => {
+        if (value === "gray") return "green"
+        if (value === "green") return "yellow"
+        if (value === "yellow") return "red"
+        if (value === "red") return "green"
       })
-      return ()=> clearInterval(value)
-    },1000)
+
+    }, 1000)
+    return () => clearInterval(value)
+  })
   return (
     <div>
-        <div className={`light ${color==="green"?"green":"gray"}`}></div>
-        <div className={`light ${color==="yellow"?"yellow":"gray"}`}></div>
-        <div className={`light ${color==="red"?"red":"gray"}`}></div>
+      <div className={`light ${color === "green" ? "green" : "gray"}`}></div>
+      <div className={`light ${color === "yellow" ? "yellow" : "gray"}`}></div>
+      <div className={`light ${color === "red" ? "red" : "gray"}`}></div>
     </div>
   )
 }
