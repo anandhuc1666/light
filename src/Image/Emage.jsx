2c4518db9a4cb197f8028588f58fe558663cc4ec
anandhuc1666 <anandhuc166@gmail.com>
2025-09-13 12:37:54 +0530

    miniCode

--- src/Image/Emage.jsx
+++ src/Image/Emage.jsx
@@ -92,46 +92,71 @@
 // }
 
 // export default Emage
-import React, { useRef, useState } from 'react'
-import svg from '../Image/vite.svg'
-import { array } from 'i/lib/util'
+// import React, { useRef, useState } from 'react'
+// import svg from '../Image/vite.svg'
+// import { array } from 'i/lib/util'
 
+// function Emage() {
+//   const [count, setCount] = useState(0)
+//   const intervel = useRef(null)
+//   const handleStart = () => {
+//     intervel.current = setInterval(() => {
+//       setCount((pre) => pre >= 5 ? 5 : pre + 1)
+
+//     }, 500)
+//   }
+//   const handleStop = () => {
+//     clearInterval(intervel.current)
+//   }
+//   const handleReset = () => {
+//     clearInterval(intervel.current)
+//     setCount(0)
+//   }
+//   let Newimag = []
+//   for (let i = 0; i < count; i++) {
+//     Newimag.push(<img src={svg} alt="" />)
+//   }
+// let numberss = count===5?5*3:null 
+//   return (
+//     <div>
+//       <h1>timer</h1>
+//       <h2>{count}</h2>
+//       <p>{numberss}</p>
+//       <p>{count===5?"number is full": null}</p>
+//       <button onClick={handleStart}>start</button>
+//       <button onClick={handleStop}>stop</button>
+//       <button onClick={handleReset}>reset</button>
+//       {
+//         Newimag.map((i) => (
+//           <div>{i}</div>
+//         ))
+//       }
+
+//     </div>
+//   )
+// }
+
+// export default Emage
+import React, { useEffect, useState } from 'react'
+import './Emage.css'
 function Emage() {
-  const [count, setCount] = useState(0)
-  const intervel = useRef(null)
-  const handleStart = () => {
-    intervel.current = setInterval(() => {
-      setCount((pre) => pre >= 5 ? 5 : pre + 1)
-
-    }, 500)
-  }
-  const handleStop = () => {
-    clearInterval(intervel.current)
-  }
-  const handleReset = () => {
-    clearInterval(intervel.current)
-    setCount(0)
-  }
-  let Newimag = []
-  for (let i = 0; i < count; i++) {
-    Newimag.push(<img src={svg} alt="" />)
-  }
-let numberss = count===5?5*3:null 
+  const [color, setColor] = useState("gray")
+  useEffect(() => {
+    let nuColor = setInterval(() => {
+      setColor((pre) => {
+        if (pre === "gray")return "green"
+        if(pre === "green")return "yellow"
+        if(pre === "yellow")return "red"
+        if(pre === "red")return "green"
+     })
+    }, 1000)
+    clearInterval(nuColor)
+  }, [])
   return (
     <div>
-      <h1>timer</h1>
-      <h2>{count}</h2>
-      <p>{numberss}</p>
-      <p>{count===5?"number is full": null}</p>
-      <button onClick={handleStart}>start</button>
-      <button onClick={handleStop}>stop</button>
-      <button onClick={handleReset}>reset</button>
-      {
-        Newimag.map((i) => (
-          <div>{i}</div>
-        ))
-      }
-
+      <div className={`light ${color === "green" ? "green" : "gray"}`}></div>
+      <div className={`light ${color === "yellow" ? "yellow" : "gray"}`}></div>
+      <div className={`light ${color === "red" ? "red" : "gray"}`}></div>
     </div>
   )
 }
