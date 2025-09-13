ca7013f89d8ffbd9cce96ecaa67f621be8250311
anandhuc1666 <anandhuc166@gmail.com>
2025-09-13 12:18:44 +0530

    MiniCode

--- src/Image/Emage.jsx
+++ src/Image/Emage.jsx
@@ -70,23 +70,69 @@
 // }
 
 // export default Emage
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
+// import svg from '../Image/vite.svg'
+// function Emage() {
+//   const [image,setImage]=useState(0)
+//   let num = []
+//   for(let i=0;i<image;i++){
+//     num.push(<img src={svg} alt="" />)
+//   }
+//   return (
+
+//     <div>
+//       <input type="text" value={image} onChange={(e)=>setImage(e.target.value)}/>
+//       {
+//         num.map((i)=>(
+//           <div>{i}</div>
+//         ))
+//       }
+//     </div>
+//   )
+// }
+
+// export default Emage
+import React, { useRef, useState } from 'react'
 import svg from '../Image/vite.svg'
+import { array } from 'i/lib/util'
+
 function Emage() {
-  const [image,setImage]=useState(0)
-  let num = []
-  for(let i=0;i<image;i++){
-    num.push(<img src={svg} alt="" />)
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
+  const intervel = useRef(null)
+  const handleStart = () => {
+    intervel.current = setInterval(() => {
+      setCount((pre) => pre >= 5 ? 5 : pre + 1)
+
+    }, 500)
   }
+  console.log(text)
+  const handleStop = () => {
+    clearInterval(intervel.current)
+  }
+  const handleReset = () => {
+    clearInterval(intervel.current)
+    setCount(0)
+  }
+  let Newimag = []
+  for (let i = 0; i < count; i++) {
+    Newimag.push(<img src={svg} alt="" />)
+  }
+console.log(count)
   return (
-    
     <div>
-      <input type="text" value={image} onChange={(e)=>setImage(e.target.value)}/>
+      <h1>timer</h1>
+      <h2>{count}</h2>
+      <p>{text}</p>
+      <button onClick={handleStart}>start</button>
+      <button onClick={handleStop}>stop</button>
+      <button onClick={handleReset}>reset</button>
       {
-        num.map((i)=>(
+        Newimag.map((i) => (
           <div>{i}</div>
         ))
       }
+
     </div>
   )
 }
