0177b34060bcbe902c5838d5157ab52dc7d9d721
anandhuc1666 <anandhuc166@gmail.com>
2025-09-13 12:41:11 +0530

    miniCode

--- src/Image/Emage.jsx
+++ src/Image/Emage.jsx
@@ -137,28 +137,28 @@
 // }
 
 // export default Emage
-import React, { useEffect, useState } from 'react'
-import './Emage.css'
-function Emage() {
-  const [color, setColor] = useState("gray")
-  useEffect(() => {
-    let nuColor = setInterval(() => {
-      setColor((pre) => {
-        if (pre === "gray")return "green"
-        if(pre === "green")return "yellow"
-        if(pre === "yellow")return "red"
-        if(pre === "red")return "green"
-     })
-    }, 1000)
-    clearInterval(nuColor)
-  }, [])
-  return (
-    <div>
-      <div className={`light ${color === "green" ? "green" : "gray"}`}></div>
-      <div className={`light ${color === "yellow" ? "yellow" : "gray"}`}></div>
-      <div className={`light ${color === "red" ? "red" : "gray"}`}></div>
-    </div>
-  )
-}
+// import React, { useEffect, useState } from 'react'
+// import './Emage.css'
+// function Emage() {
+//   const [color, setColor] = useState("gray")
+//   useEffect(() => {
+//     let nuColor = setInterval(() => {
+//       setColor((pre) => {
+//         if (pre === "gray")return "green"
+//         if(pre === "green")return "yellow"
+//         if(pre === "yellow")return "red"
+//         if(pre === "red")return "green"
+//      })
+//     }, 1000)
+//    return()=> clearInterval(nuColor)
+//   }, [])
+//   return (
+//     <div>
+//       <div className={`light ${color === "green" ? "green" : "gray"}`}></div>
+//       <div className={`light ${color === "yellow" ? "yellow" : "gray"}`}></div>
+//       <div className={`light ${color === "red" ? "red" : "gray"}`}></div>
+//     </div>
+//   )
+// }
 
-export default Emage
+// export default Emage
