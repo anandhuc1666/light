42405fec591b8c4fa3780fb8fce9691c898b35ed
anandhuc1666 <anandhuc166@gmail.com>
2025-09-15 10:05:41 +0530

    CodeBuild

--- src/Image/Emage.jsx
+++ src/Image/Emage.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from 'react'
+import './Emage.css'
 
 function Emage() {
     const [color, setColor] = useState('gray')
+    const [count, setCount] = useState(1)
     useEffect(() => {
         const colors = setInterval(() => {
             setColor((pre) => {
-                if (pre === "gray") return "green"
+                  if (pre === "gray") {
+                     return "green"
+        }
                 if (pre === "green") return "yellow"
                 if (pre === "yellow") return "red"
                 if (pre === "red") return "green"
+
             })
+
         }, 1000)
         return () => clearInterval(colors)
     }, [])
+
     return (
         <div>
-            <div className={`light ${color === "green" ? "green" : "gray"}`}></div>
-            <div className={`light ${color === "yellow" ? "yellow" : "gray"}`}></div>
-            <div className={`light ${color === "red" ? "red" : "gray"}`}></div>
+            <div className={`light ${color === "green" ? "green" : "gray"}`}><h2></h2></div>
+            <div className={`light ${color === "yellow" ? "yellow" : "gray"}`}><h2></h2></div>
+            <div className={`light ${color === "red" ? "red" : "gray"}`}><h2></h2></div>
         </div>
     )
 }
