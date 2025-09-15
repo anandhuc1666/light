a43a365e9c8702f88970c2c4fe8241ad9b0f4af5
anandhuc1666 <anandhuc166@gmail.com>
2025-09-15 11:01:29 +0530

    CodeBuild

--- src/Image/Emage.jsx
+++ src/Image/Emage.jsx
@@ -1,55 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import './Emage.css'
+// import React, { useEffect, useRef, useState } from 'react'
+// import './Emage.css'
+
+// function Emage() {
+//     const [color, setColor] = useState('gray')
+//     const [yellow, setYellow] = useState(0)
+//     const [red, setRed] = useState(0)
+//     const [count, setCount] = useState(0)
+
+//     useEffect(() => {
+//         const colors = setInterval(() => {
+//             setColor((pre) => {
+//                 if (pre === "gray") {
+//                     return ("green")
+//                 }
+//                 if (pre === "green") return "yellow"
+//                 if (pre === "yellow") return "red"
+//                 if (pre === "red") return "green"
+
+//             })
+
+
+//         }, 1000)
+//         return () => clearInterval(colors)
+
+//     }, [])
+//     const handle = () => {
+//         setColor("gray")
+//         setCount(0)
+//         setYellow((pre) => pre ? pre-1 : 0)
+//         setRed(0)
+//     }
+//     useEffect(() => {
+//         if (color === "green") {
+//             setCount((pre) => pre + 1)
+//         }
+//         if (color === "yellow") {
+//             setYellow((pre) => pre + 1)
+//         }
+//         if (color === "red") {
+//             setRed((r) => r + 1)
+//         }
+//     }, [color])
+
+//     return (
+//         <div>
+//             <div className={`light ${color === "green" ? "green" : "gray"}`}><h2>{count}</h2></div>
+//             <div className={`light ${color === "yellow" ? "yellow" : "gray"}`}><h2>{yellow}</h2></div>
+//             <div className={`light ${color === "red" ? "red" : "gray"}`}><h2>{red}</h2></div>
+//             <button onClick={handle}>reset</button>
+//         </div>
+//     )
+// }
+
+// export default Emage
+
+import React from 'react'
 
 function Emage() {
-    const [color, setColor] = useState('gray')
-    const [yellow, setYellow] = useState(0)
-    const [red, setRed] = useState(0)
-    const [count, setCount] = useState(0)
-
-    useEffect(() => {
-        const colors = setInterval(() => {
-            setColor((pre) => {
-                if (pre === "gray") {
-                    return ("green")
-                }
-                if (pre === "green") return "yellow"
-                if (pre === "yellow") return "red"
-                if (pre === "red") return "green"
-
-            })
-
-
-        }, 1000)
-        return () => clearInterval(colors)
-
-    }, [])
-    const handle = () => {
-        setColor("gray")
-        setCount(0)
-        setYellow((pre) => pre ? pre-1 : 0)
-        setRed(0)
-    }
-    useEffect(() => {
-        if (color === "green") {
-            setCount((pre) => pre + 1)
-        }
-        if (color === "yellow") {
-            setYellow((pre) => pre + 1)
-        }
-        if (color === "red") {
-            setRed((r) => r + 1)
-        }
-    }, [color])
-
-    return (
-        <div>
-            <div className={`light ${color === "green" ? "green" : "gray"}`}><h2>{count}</h2></div>
-            <div className={`light ${color === "yellow" ? "yellow" : "gray"}`}><h2>{yellow}</h2></div>
-            <div className={`light ${color === "red" ? "red" : "gray"}`}><h2>{red}</h2></div>
-            <button onClick={handle}>reset</button>
-        </div>
-    )
+  return (
+    <div>Emage</div>
+  )
 }
 
 export default Emage
---
+++ src/Image/Emage2.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Emage2() {
+  return (
+    <div>Emage2</div>
+  )
+}
+
+export default Emage2
