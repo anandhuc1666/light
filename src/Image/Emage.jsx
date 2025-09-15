942de217b0ad1f978069cb200ad2f1e569434165
anandhuc1666 <anandhuc166@gmail.com>
2025-09-15 09:48:45 +0530

    MiniCode

--- src/Image/Emage.jsx
+++ src/Image/Emage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Emage() {
-  return (
-    <div>Emage</div>
-  )
+    const [color, setColor] = useState('gray')
+    useEffect(() => {
+        const colors = setInterval(() => {
+            setColor((pre) => {
+                if (pre === "gray") return "green"
+                if (pre === "green") return "yellow"
+                if (pre === "yellow") return "red"
+                if (pre === "red") return "green"
+            })
+        }, 1000)
+        return () => clearInterval(colors)
+    }, [])
+    return (
+        <div>
+            <div className={`light ${color === "green" ? "green" : "gray"}`}></div>
+            <div className={`light ${color === "yellow" ? "yellow" : "gray"}`}></div>
+            <div className={`light ${color === "red" ? "red" : "gray"}`}></div>
+        </div>
+    )
 }
 
 export default Emage
