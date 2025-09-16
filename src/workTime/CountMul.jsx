a477dcc6a8ffe03999b0cf80c51a6b521e098f5e
anandhuc1666 <anandhuc166@gmail.com>
2025-09-16 11:29:31 +0530

    codeBuild

--- src/workTime/CountMul.jsx
+++ src/workTime/CountMul.jsx
@@ -3,14 +3,21 @@ import React, { useEffect, useRef, useState } from 'react'
 function CountMul() {
     const [count, setCount] = useState(0)
     const intervel = useRef(null)
+    const handle = () => {
+        intervel.current = setInterval(() => {
+            setCount((pre) => pre + 1)
+        }, 1000)
+    }
 
-  
+    const stop = () => {
+        clearInterval(intervel.current)
+    }
 
     return (
         <div>
             <h2>{count}</h2>
             <button onClick={handle}>+</button>
-            <button onClick={()=>stop()}>stop</button>
+            <button onClick={() => stop()}>stop</button>
         </div>
     )
 }
