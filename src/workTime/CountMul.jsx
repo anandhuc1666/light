8bd1f921d248f3ebf168b7f48033d26885333a77
anandhuc1666 <anandhuc166@gmail.com>
2025-09-16 12:20:42 +0530

    CodeBuild

--- src/workTime/CountMul.jsx
+++ src/workTime/CountMul.jsx
@@ -1,23 +1,44 @@
-import React, { useEffect, useRef, useState } from 'react'
+// import React, { useEffect, useRef, useState } from 'react'
+
+// function CountMul() {
+//     const [count, setCount] = useState(0)
+//     const intervel = useRef(null)
+//     const handle = () => {
+//         intervel.current = setInterval(() => {
+//             setCount((pre) => pre + 1)
+//         }, 1000)
+//     }
+
+//     const stop = () => {
+//         clearInterval(intervel.current)
+//     }
+
+//     return (
+//         <div>
+//             <h2>{count}</h2>
+//             <button onClick={handle}>+</button>
+//             <button onClick={() => stop()}>stop</button>
+//         </div>
+//     )
+// }
+
+// export default CountMul
+import React, { useState } from 'react'
 
 function CountMul() {
-    const [count, setCount] = useState(0)
-    const intervel = useRef(null)
-    const handle = () => {
-        intervel.current = setInterval(() => {
-            setCount((pre) => pre + 1)
-        }, 1000)
+    const [state, setState] = useState({
+        name: "arun",
+        age: "20"
+    })
+    let person = []
+    const handle =()=>{
+        person.push(...state,)
     }
-
-    const stop = () => {
-        clearInterval(intervel.current)
-    }
-
     return (
         <div>
-            <h2>{count}</h2>
-            <button onClick={handle}>+</button>
-            <button onClick={() => stop()}>stop</button>
+            <i>{state}</i>
+            <input type="text" onChange={(e) => setState(e.target.value)} />
+            <button onClick={handle}>send</button>
         </div>
     )
 }
