c3a6635c0f038f691f487fe425962f3948b89e1d
anandhuc1666 <anandhuc166@gmail.com>
2025-09-17 11:46:19 +0530

    CodeBuild

--- src/ColorCode/Colorlight.jsx
+++ src/ColorCode/Colorlight.jsx
@@ -3,12 +3,13 @@ import './Colorlight.css'
 
 function Colorlight() {
     const[color,setColor]=useState('gray')
+    
   return (
     <div>
         <h1>light</h1>
         <div className={`light ${color? "green":"gray"}`}></div>
         <div className={`light ${color? "yellow" : "gray"}`}></div>
-        <div className={`light ${}`}></div>
+        <div className={`light ${color? "red" : "gray"}`}></div>
     </div>
   )
 }
