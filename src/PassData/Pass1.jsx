d3fa07cd7805b9ee4a5874bb77fbbf6a0d7346bc
anandhuc1666 <anandhuc166@gmail.com>
2025-09-17 12:27:59 +0530

    CodeBuild

--- src/PassData/Pass1.jsx
+++ src/PassData/Pass1.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { TextContext } from './CreateContext'
 
 function Pass1() {
-    const {text,setText}=useContext(TextContext)
+     const {text,setText}=useContext(TextContext)
   return (
     <div>
         <h2>{text}</h2>
