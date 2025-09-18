d9a4d110bcedb5ea05e5112cbb621f1160ce15ca
anandhuc1666 <anandhuc166@gmail.com>
2025-09-18 10:56:48 +0530

    CodeBuild

--- src/TimeCount/FirstCount.jsx
+++ src/TimeCount/FirstCount.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useState } from 'react'
 
 function FirstCount() {
-  return (
-    <div>
-        
-    </div>
-  )
+    const [count, setCount] = useState(0)
+    let newTimer = useRef(null)
+    const handle = () => {
+        newTimer = setInterval(() => {
+            setCount((pre) => {
+                pre + 1
+            })
+        }, 1000)
+    }
+const stop =()=>{
+    clearInterval(newTimer)
+}
+
+    return (
+        <div>
+            <h2>{count}</h2>
+            <button onClick={handle}>start</button>
+            <button onClick={stop}>stop</button>
+        </div>
+    )
 }
 
 export default FirstCount
